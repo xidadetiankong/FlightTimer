7f26cc1eeb5113c4bf8cb16f21af0944a71a5f25
xidadetiankong <xidadetiankong@users.noreply.github.com>
2020-04-24 20:05:49 +0800

    增加了账户删除功能

--- miniprogram/pages/editUserInfo/editUserInfo.js
+++ miniprogram/pages/editUserInfo/editUserInfo.js
@@ -29,10 +29,46 @@ Page({
       avatarUrl: app.userInfo.avatarUrl,
       nickName: app.userInfo.nickName,
       signature: app.userInfo.signature,
-      profession: app.userInfo.profession
+      profession: app.userInfo.profession,
+      openId: app.userInfo._openid
     })
     console.log(app.userInfo)
   },
-  
+  closeAccount: function () {
+    var that = this
+    wx.showModal({
+      content: '账户注销后所有数据都将会删除，确认要继续么(┬＿┬)？',
+      confirmColor: '#FFAABB',
+      success(res) {
+        if (res.confirm) {
+          db.collection('timeData').where({
+            _openid: that.data.openId
+          }).remove({
+            success: function (res) {
+              db.collection('userprofile').where({
+                _openid: that.data.openId
+              }).remove({
+
+                success: function (res) {
+                  let userInfo = {}
+                  app.userInfo = Object.assign(app.userInfo, userInfo)
+                 
+                  wx.reLaunch({
+                    url: '../echo/echo',
+                  })
+                }
+              })
+              console.log(res.data)
+            }
+          })
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+
+    })
+  },
+
 
 })
