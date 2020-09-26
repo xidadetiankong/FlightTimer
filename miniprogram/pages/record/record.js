e98c1d28b710e7304e43d24661678ff300ab0f85
xidadetiankong <54253417+xidadetiankong@users.noreply.github.com>
2020-09-26 17:16:53 +0800

    增加流量主广告展示

--- miniprogram/pages/record/record.js
+++ miniprogram/pages/record/record.js
@@ -44,7 +44,27 @@ profession:''
       path: '/page/user?id=123'
     }
   },
- 
+ onLoad:function(){
+   // 在页面中定义插屏广告
+let interstitialAd = null
+
+// 在页面onLoad回调事件中创建插屏广告实例
+if (wx.createInterstitialAd) {
+  interstitialAd = wx.createInterstitialAd({
+    adUnitId: 'adunit-ef79a9727360f28c'
+  })
+  interstitialAd.onLoad(() => {})
+  interstitialAd.onError((err) => {})
+  interstitialAd.onClose(() => {})
+}
+
+// 在适合的场景显示插屏广告
+if (interstitialAd) {
+  interstitialAd.show().catch((err) => {
+    console.error(err)
+  })
+}
+ },
  
   
 
